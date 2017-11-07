a79400e29b4f7f131c0a858cbacf17d24a3c0fa1
GSoster <guilhermesosters@gmail.com>
2017-11-07 12:19:55 -0200

    Added project Getting Started with ASP.NET MVC 5

---
+++ mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Getting_Started_with_ASP.NET_MVC_5.Startup))]
+namespace Getting_Started_with_ASP.NET_MVC_5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
