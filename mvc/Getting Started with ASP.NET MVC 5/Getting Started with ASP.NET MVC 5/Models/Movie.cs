d57c9043a1720bf9a28abc75ccd795eea2dcf08a
GSoster <guilhermesosters@gmail.com>
2017-11-08 14:34:46 -0200

    Updated Movie class with data annotations

--- mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Models/Movie.cs
+++ mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Getting_Started_with_ASP.NET_MVC_5.Models
 {
@@ -7,6 +8,10 @@ namespace Getting_Started_with_ASP.NET_MVC_5.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")] //The Display attribute specifies what to display for the name of a field
+        [DataType(DataType.Date)] //The DataType attribute specifies the type of the data, in this case it's a date (time info will not be displayed)
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] //The DisplayFormat attribute is needed for a bug in the Chrome browser that renders date formats incorrectly
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
