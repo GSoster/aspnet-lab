f129c6c81aadae96d4b8e381b7d5960b8db63d7b
GSoster <guilhermesosters@gmail.com>
2017-11-08 09:47:23 -0200

    Added movie class

---
+++ mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Getting_Started_with_ASP.NET_MVC_5.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
