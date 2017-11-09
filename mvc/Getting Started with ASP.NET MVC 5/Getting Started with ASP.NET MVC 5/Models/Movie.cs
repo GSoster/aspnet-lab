a6e474298ebd6d9ec44f8d0e06572796466cbf50
GSoster <guilhermesosters@gmail.com>
2017-11-09 14:38:27 -0200

    Added data annotations to validate the Movie class

--- mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Models/Movie.cs
+++ mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Models/Movie.cs
@@ -7,16 +7,29 @@ namespace Getting_Started_with_ASP.NET_MVC_5.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")] //The Display attribute specifies what to display for the name of a field
         [DataType(DataType.Date)] //The DataType attribute specifies the type of the data, in this case it's a date (time info will not be displayed)
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] //The DisplayFormat attribute is needed for a bug in the Chrome browser that renders date formats incorrectly
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
+
 
     public class MovieDBContext : DbContext
     {
