e5b50fa327097783029abfcba4f2997d5ae632d7
GSoster <guilhermesosters@gmail.com>
2017-11-09 10:55:53 -0200

    Added migrations to the project and filled the seed method of Migrations/configuration.cs

---
+++ mvc/Getting Started with ASP.NET MVC 5/Getting Started with ASP.NET MVC 5/Migrations/Configuration.cs
@@ -0,0 +1,51 @@
+namespace Getting_Started_with_ASP.NET_MVC_5.Migrations
+{
+    using Getting_Started_with_ASP.NET_MVC_5.Models;
+    using System;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Getting_Started_with_ASP.NET_MVC_5.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Getting_Started_with_ASP.NET_MVC_5.Models.MovieDBContext context)
+        {
+            context.Movies.AddOrUpdate(i => i.Title,
+        new Movie
+        {
+            Title = "When Harry Met Sally",
+            ReleaseDate = DateTime.Parse("1989-1-11"),
+            Genre = "Romantic Comedy",
+            Price = 7.99M
+        },
+
+         new Movie
+         {
+             Title = "Ghostbusters ",
+             ReleaseDate = DateTime.Parse("1984-3-13"),
+             Genre = "Comedy",
+             Price = 8.99M
+         },
+
+         new Movie
+         {
+             Title = "Ghostbusters 2",
+             ReleaseDate = DateTime.Parse("1986-2-23"),
+             Genre = "Comedy",
+             Price = 9.99M
+         },
+
+       new Movie
+       {
+           Title = "Rio Bravo",
+           ReleaseDate = DateTime.Parse("1959-4-15"),
+           Genre = "Western",
+           Price = 3.99M
+       }
+   );
+        }
+    }
+}
